7759d5f2b63a94bc86253c08e738b7c6bc3f6781
ak2513001-ai <ak2513001@gmail.com>
2025-10-01 16:41:31 +0530

    Update hd.js

--- hd.js
+++ hd.js
@@ -1,40 +1,47 @@
 (function(){
-    window.geofsNewHDState = true;
-    window.geofs.geoIpUpdate = function() {
+    window.geofsNewHDState=true;
+    window.geofs.geoIpUpdate=function(){
         delete window.geofs.api.analytics;
         document.body.classList.add("geofs-hd");
-        window.geofs.api.imageryProvider = new Cesium.UrlTemplateImageryProvider({
-            maximumLevel: 21,
-            hasAlphaChannel: false,
-            subdomains: ["mt0", "mt1", "mt2", "mt3"],
-            url: "https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+        window.geofs.api.imageryProvider=new Cesium.UrlTemplateImageryProvider({
+            maximumLevel:21,
+            hasAlphaChannel:false,
+            subdomains:["mt0","mt1","mt2","mt3"],
+            url:"https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
         });
-        window.geofs.api.setImageryProvider(window.geofs.api.imageryProvider, false);
-        window.geofs.api.viewer.terrainProvider = window.geofs.api.flatRunwayTerrainProviderInstance = new geofs.api.FlatRunwayTerrainProvider({
-            baseProvider: new Cesium.CesiumTerrainProvider({
-                url: "https://data.geo-fs.com/srtm/",
-                requestWaterMask: false,
-                requestVertexNormals: true
+        window.geofs.api.setImageryProvider(window.geofs.api.imageryProvider,false);
+        window.geofs.api.viewer.terrainProvider=window.geofs.api.flatRunwayTerrainProviderInstance=new geofs.api.FlatRunwayTerrainProvider({
+            baseProvider:new Cesium.CesiumTerrainProvider({
+                url:"https://data.geo-fs.com/srtm/",
+                requestWaterMask:false,
+                requestVertexNormals:true
             }),
-            bypass: false,
-            maximumLevel: 12
-        });
+            bypass:false,
+            maximumLevel:12
+        })
     };
-    window.executeOnEventDone("geofsStarted", function() {
-        if (window.geofs.api.hdOn === window.geofsNewHDState) return;
+    window.executeOnEventDone("geofsStarted",function(){
+        if(window.geofs.api.hdOn===window.geofsNewHDState)return;
         $("body").trigger("terrainProviderWillUpdate");
         window.geofs.geoIpUpdate();
-        window.geofs.api.hdOn = window.geofsNewHDState;
+        window.geofs.api.hdOn=window.geofsNewHDState;
         window.geofs.api.renderingQuality();
         $("body").trigger("terrainProviderUpdate");
-        var removeAds = function() {
+        var removeAds=function(){
             document.querySelectorAll("iframe[src*='ads'],iframe[src*='doubleclick'],iframe[src*='googlesyndication']").forEach(function(ad){ad.remove()});
-            document.querySelectorAll("div[id^='google_ads'],ins.adsbygoogle,#ads,#adContainer,#ad-banner,.ad-container,.banner-ads%22").forEach(function(ad){ad.style.display='none'});
+            document.querySelectorAll("div[id^='google_ads'],ins.adsbygoogle,#ads,#adContainer,#ad-banner,.ad-container,.banner-ads%22).forEach(function(ad){ad.style.display='none'});
         };
         removeAds();
-        setInterval(removeAds, 3000);
+        setInterval(removeAds,3000)
     });
-    window.executeOnEventDone("afterDeferredload", function() {
-        window.geofs.mapXYZ = "https://data.geo-fs.com/osm/{z}/{x}/{y}.png";
+    window.executeOnEventDone("afterDeferredload",function(){
+        window.geofs.mapXYZ="https://data.geo-fs.com/osm/{z}/{x}/{y}.png"
     });
+    function removeElementsByClass(className){
+        const elements=document.getElementsByClassName(className);
+        while(elements.length>0){
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    }
+    removeElementsByClass("geofs-adbanner geofs-adsense-container");
 })();
